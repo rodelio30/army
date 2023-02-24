2dfe6e1fda99b987fb61446679270d8c332f7092
Rodelio <rodeliobdomingojr@gmail.com>
2023-02-24 11:40:21 +0800

    initial for bulletin board

--- bulletin.php
+++ bulletin.php
@@ -39,9 +39,11 @@
   <body>
     
 <header>
-  <nav class="navbar navbar-expand-md navbar-dark fixed-top bg-success">
+  <nav class="navbar navbar-expand-md navbar-dark fixed-top bg-navigation">
     <div class="container">
-      <a class="navbar-brand" href="bulletin.php">ARMY</a>
+      <a class="navbar-brand" href="bulletin.php">
+        <img src="img/icons/army_logo.png" style="heigth: 13rem; width: 13rem;" alt="Sunset Over the City"/>
+      </a>
       <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarCollapse" aria-controls="navbarCollapse" aria-expanded="false" aria-label="Toggle navigation">
         <span class="navbar-toggler-icon"></span>
       </button>
