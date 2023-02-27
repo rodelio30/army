15e90fb3529e7a018a47d539b190ac1f8c6bcfbf
Rodelio <rodeliobdomingojr@gmail.com>
2023-02-27 11:24:40 +0800

    added a footer

--- bulletin.php
+++ bulletin.php
@@ -1,159 +1,61 @@
-<!doctype html>
+<!DOCTYPE html>
 <html lang="en">
-  <head>
-    <meta charset="utf-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1">
-    <meta name="description" content="">
-    <meta name="author" content="Mark Otto, Jacob Thornton, and Bootstrap contributors">
-    <meta name="generator" content="Hugo 0.84.0">
-    <title>ARMY</title>
-    <link rel="shortcut icon" href="img/icons/army_icon.png" />
 
-    <!-- <link rel="canonical" href="https://getbootstrap.com/docs/5.0/examples/carousel/"> -->
+<?php include 'header_main.php' ?>
 
-    
+<body style="background-color: white;">
+    <div class="wrapper">
 
-    <!-- Bootstrap core CSS -->
-<link href="assets/dist/css/bootstrap.min.css" rel="stylesheet">
-
-    <style>
-      .bd-placeholder-img {
-        font-size: 1.125rem;
-        text-anchor: middle;
-        -webkit-user-select: none;
-        -moz-user-select: none;
-        user-select: none;
-      }
-
-      @media (min-width: 768px) {
-        .bd-placeholder-img-lg {
-          font-size: 3.5rem;
-        }
-      }
-    </style>
-
-    
-    <!-- Custom styles for this template -->
-    <link href="css/carousel.css" rel="stylesheet">
-  </head>
-  <body>
-    
-  <?php include 'bulletin_header.php'?>
-
-<main>
-
-  <div id="myCarousel" class="carousel slide" data-bs-ride="carousel">
-    <div class="carousel-indicators">
-      <button type="button" data-bs-target="#myCarousel" data-bs-slide-to="0" class="active" aria-current="true" aria-label="Slide 1"></button>
-      <button type="button" data-bs-target="#myCarousel" data-bs-slide-to="1" aria-label="Slide 2"></button>
-      <button type="button" data-bs-target="#myCarousel" data-bs-slide-to="2" aria-label="Slide 3"></button>
-      <button type="button" data-bs-target="#myCarousel" data-bs-slide-to="3" aria-label="Slide 4"></button>
-    </div>
-    <div class="carousel-inner">
-      <div class="carousel-item active">
-        <!-- <svg class="bd-placeholder-img" width="100%" height="100%" xmlns="img/caro/caro1.jpg" aria-hidden="true" preserveAspectRatio="xMidYMid slice" focusable="false"><rect width="100%" height="100%" fill="#777"/></svg> -->
-        <img src="img/caro/caro1.jpg" class="d-block w-100" alt="Sunset Over the City"/>
-
-        <!-- <div class="container">
-          <div class="carousel-caption text-start">
-            <h1>Example headline.</h1>
-            <p>Some representative placeholder content for the first slide of the carousel.</p>
-            <p><a class="btn btn-lg btn-primary" href="#">Sign up today</a></p>
-          </div>
-        </div> -->
-      </div>
-      <div class="carousel-item">
-        <!-- <svg class="bd-placeholder-img" width="100%" height="100%" xmlns="http://www.w3.org/2000/svg" aria-hidden="true" preserveAspectRatio="xMidYMid slice" focusable="false"><rect width="100%" height="100%" fill="#777"/></svg> -->
-        <img src="img/caro/caro2.jpg" class="d-block w-100" alt="Sunset Over the City"/>
-
-      </div>
-      <div class="carousel-item">
-        <!-- <svg class="bd-placeholder-img" width="100%" height="100%" xmlns="http://www.w3.org/2000/svg" aria-hidden="true" preserveAspectRatio="xMidYMid slice" focusable="false"><rect width="100%" height="100%" fill="#777"/></svg> -->
-        <img src="img/caro/caro3.jpg" class="d-block w-100" alt="Sunset Over the City"/>
-
-      </div>
-      <div class="carousel-item">
-        <!-- <svg class="bd-placeholder-img" width="100%" height="100%" xmlns="http://www.w3.org/2000/svg" aria-hidden="true" preserveAspectRatio="xMidYMid slice" focusable="false"><rect width="100%" height="100%" fill="#777"/></svg> -->
-        <img src="img/caro/caro4.jpg" class="d-block w-100" alt="Sunset Over the City"/>
-
-      </div>
-    </div>
-    <button class="carousel-control-prev" type="button" data-bs-target="#myCarousel" data-bs-slide="prev">
-      <span class="carousel-control-prev-icon" aria-hidden="true"></span>
-      <span class="visually-hidden">Previous</span>
-    </button>
-    <button class="carousel-control-next" type="button" data-bs-target="#myCarousel" data-bs-slide="next">
-      <span class="carousel-control-next-icon" aria-hidden="true"></span>
-      <span class="visually-hidden">Next</span>
-    </button>
-  </div>
-
-
-  <!-- Marketing messaging and featurettes
-  ================================================== -->
-  <!-- Wrap the rest of the page in another container to center all the content. -->
-
-  <div class="container marketing">
-    <!-- START THE ANNOUNCEMENTS -->
-  <?php
-  $conn = mysqli_connect("localhost", "root", "");
-  if (!$conn) {
-    $ConnErr = "Not Connected to the Server";
-  }
-  if (!mysqli_select_db($conn, 'army')) {
-    $SelcErr = "Database Not Selected";
-  }
-      $result = mysqli_query($conn, "select img, title, description from announcements WHERE status != 'archive' ORDER BY date_modified") or die("Query for latest reservist....");
-      while (list($img, $title, $description) = mysqli_fetch_array($result)) {
-          echo "
-        <div class='row featurette'>
-          <div class='col-md-7 order-md-2'>
-            <h2 class='featurette-heading'>$title</h2>
-            <p class='lead'>$description</p>
-          </div>
-          <div class='col-md-5 order-md-1'>
-            <svg class='bd-placeholder-img bd-placeholder-img-lg featurette-image img-fluid mx-auto' width='500' height='500' xmlns='img/announce/$img' role='img' aria-label='Placeholder: 500x500' preserveAspectRatio='xMidYMid slice' focusable='false'><title>Placeholder</title><rect width='100%' height='100%' fill='#eee'/><text x='50%' y='50%' fill='#aaa' dy='.3em'>500x500</text></svg>
-
-          </div>
+        <div class="main">
+            <?php include 'bulletin_navigation.php' ?>
+            <?php include 'bulletin_carousel.php'?>
+    <div class="container marketing">
+    <div class="card">
+        <div class="card-header">
+            <h1><strong>Latest Announcement</strong> </h1>
+        <!-- <h1 class="mb-4">Latest Announcement</h1> -->
         </div>
-      ";
-      }
-  ?>
-    <hr class="featurette-divider">
+        <div class="card-body">
+                <!-- START THE ANNOUNCEMENTS -->
+            <?php
+            $conn = mysqli_connect("localhost", "root", "");
+            if (!$conn) {
+                $ConnErr = "Not Connected to the Server";
+            }
+            if (!mysqli_select_db($conn, 'army')) {
+                $SelcErr = "Database Not Selected";
+            }
+                $result = mysqli_query($conn, "select img, title, description from announcements WHERE status != 'archive' ORDER BY date_modified") or die("Query for latest reservist....");
+                while (list($img, $title, $description) = mysqli_fetch_array($result)) {
+                    echo "
+                    <div class='row featurette'>
+                    <div class='col-md-7 order-md-2'>
+                        <h2 class='featurette-heading'>$title</h2>
+                        <p class='lead'>$description</p>
+                    </div>
+                    <div class='col-md-5 order-md-1'>
+                        <svg class='bd-placeholder-img bd-placeholder-img-lg featurette-image img-fluid mx-auto' width='500' height='500' xmlns='img/announce/$img' role='img' aria-label='Placeholder: 500x500' preserveAspectRatio='xMidYMid slice' focusable='false'><title>Placeholder</title><rect width='100%' height='100%' fill='#eee'/><text x='50%' y='50%' fill='#aaa' dy='.3em'>500x500</text></svg>
 
-    <div class="row featurette">
-      <div class="col-md-7 order-md-2">
-        <h2 class="featurette-heading">Oh yeah, it’s that good. <span class="text-muted">See for yourself.</span></h2>
-        <p class="lead">Another featurette? Of course. More placeholder content here to give you an idea of how this layout would work with some actual real-world content in place.</p>
-      </div>
-      <div class="col-md-5 order-md-1">
-        <svg class="bd-placeholder-img bd-placeholder-img-lg featurette-image img-fluid mx-auto" width="500" height="500" xmlns="http://www.w3.org/2000/svg" role="img" aria-label="Placeholder: 500x500" preserveAspectRatio="xMidYMid slice" focusable="false"><title>Placeholder</title><rect width="100%" height="100%" fill="#eee"/><text x="50%" y="50%" fill="#aaa" dy=".3em">500x500</text></svg>
+                    </div>
+                    </div>
+                    <br>
 
-      </div>
+                    <hr class='featurette-divider'>
+                ";
+                }
+            ?>
+                <!-- /END THE FEATURETTES -->
+
+                    </div><!-- /.container -->
+                </div>
+            </div>
+        </div>
     </div>
-
-    <hr class="featurette-divider">
-
-    <!-- /END THE FEATURETTES -->
-
-  </div><!-- /.container -->
-
-
-  <!-- FOOTER -->
-  <footer class="container">
-    <p class="float-end"> 
-      <a href="#">
-      <img src="img/icons/top.png" style="heigth: 3rem; width:3rem;" alt="Sunset Over the City"/>
-      </a>
-    </p>
-    <!-- <button id="myBtn"><a href="#top" style="color: white">Top</a></button> -->
-    <p>&copy; ARMY &middot; <a href="#">Privacy</a> &middot; <a href="#">Terms</a></p>
-  </footer>
-</main>
-
-
+            <?php include 'bulletin_footer.php' ?>
+    
+    <script src="js/app.js"></script>
     <script src="assets/dist/js/bootstrap.bundle.min.js"></script>
 
-      
-  </body>
+</body>
+
 </html>
