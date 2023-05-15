f14aee900f24086d71dfb0aa2d5b5c5cda6bd80a
Rodelio <rodeliobdomingojr@gmail.com>
2023-05-15 14:07:28 +0800

    add stat f in admin, remove count of reservist in admin and staff analytic dashboard

--- admin_dashboard_staff.php
+++ admin_dashboard_staff.php
@@ -18,22 +18,7 @@ $ts = $trainings_counter + $seminars_counter;
     <div class="col-xl-12 col-xxl-12 d-flex">
         <div class="w-100">
             <div class="row">
-                <div class="col-sm-4">
-                    <a href="admin_rg.php" class="linked-navigation">
-                    <div class="card">
-                        <div class="card-body">
-                            <div class="row">
-                                <div class="col mt-0">
-                                    <h5 class="card-title">Reservist</h5>
-                                </div>
-                            </div>
-                            <h1 class="mt-3 mb-1"><?php echo $reserve_counter ;?></h1>
-                        </div>
-                    </div>
-
-                    </a>
-                </div>
-                <div class="col-sm-4">
+                <div class="col-sm-6">
                     <a href="admin_trainings.php" class="linked-navigation">
                     <div class="card">
                         <div class="card-body">
@@ -47,7 +32,7 @@ $ts = $trainings_counter + $seminars_counter;
                     </div>
                     </a>
                 </div>
-                <div class="col-sm-4">
+                <div class="col-sm-6">
                     <a href="admin_announcements.php" class="linked-navigation">
                     <div class="card">
                         <div class="card-body">
--- admin_dashboard_stat_adstaff.php
+++ admin_dashboard_stat_adstaff.php
@@ -60,4 +60,17 @@ if($isAdmin){
         </div>
     </div>
 </div>
+<div class="row">
+    <div class="col-xl-12 col-xxl-12 d-flex">
+        <div class="w-100">
+            <div class="row">
+                <div class="col-12 col-lg-12 col-xxl-12 d-flex">
+                    <div class="card flex-fill">
+                        <?php include 'admin_dashboard_stat_f.php'?>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
 <?php } ?>
